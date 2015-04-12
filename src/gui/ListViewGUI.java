55f24fda9a50b2d4fe9b6e08e866c44a774894ea
darentzl <darentzl@gmail.com>
2015-04-12 15:08:23 +0800

    Icon is back

--- src/gui/ListViewGUI.java
+++ src/gui/ListViewGUI.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -23,6 +24,7 @@ public class ListViewGUI extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		this.primaryStage = primaryStage;
 		this.primaryStage.initStyle(StageStyle.UNDECORATED);	//removes default windows border
+        this.primaryStage.getIcons().add(new Image("file:images/icon.png"));
 
 		initRootLayout();
 		showTaskOverview();
